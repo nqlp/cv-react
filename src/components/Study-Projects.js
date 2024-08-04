d890a27e7823faa766cbb21f003b31ec6e4ce88b
nqlp <nqlpaul@gmail.com>
2024-08-04 12:06:21 -0400

    making code cleaner

--- src/components/Study-Projects.js
+++ src/components/Study-Projects.js
@@ -2,52 +2,53 @@ import React from "react";
 
 const StudyProjects = () => {
     return (
-    <div id="study-projects-tab" class="tab">
-        <section class="study-projects">
-            <h1>PROJETS D'ÉTUDES</h1>
-            <hr />
-            <h2>Été 2023 — Prototypage de Moodle</h2>
-            <ul>
-                <li>Implémenter des tests avec Jest;</li>
-                <li>Programmer en TypeScript / JavaScript;</li>
-                <li>Utiliser Node.js pour le côté serveur;</li>
-                <li>Collaborer et partager le code avec les collègues via GitHub.</li>
-            </ul>
-            <h2>Hiver 2023 — TripleVue</h2>
-            <ul>
-                <li>Implémenter des options de translation, zoom, glisser et déposer pour les images; </li>
-                <li>Créer une barre de menu.</li>
-            </ul>
-            <h2>Hiver 2022 — Gestion des opérations maintenances</h2>
-            <ul>
-                <li>Insérer des données avec SQL;</li>
-                <li>Développer avec PL/SQL;</li>
-                <li>Utiliser Oracle SQL Developer;</li>
-                <li>Utiliser DataGrip;</li>
-                <li>Créer des scripts SQL.</li>
-            </ul>
-            <h2>Hiver 2022 — Prototypage de jeu de rôle</h2>
-            <ul>
-                <li>Générer un labyrinthe;</li>
-                <li>Créer une interface graphique;</li>
-                <li>Programmer en Java.</li>
-            </ul>
-            <h2>Hiver 2022 — Automatisation d'une grue</h2>
-            <ul>
-                <li>Modéliser des pièces avec SolidWorks;</li>
-                <li>Automatiser d'une grue en langage C;</li>
-                <li>Utiliser d'AutoCAD pour la découpe au laser;</li>
-                <li>Configurer la manette de contrôle pour la grue;</li>
-                <li>Collaborer avec des partenaires de différents domaines pour favoriser
-                    l'apprentissage
-                    collectif.</li>
-            </ul>
-            <h2>Automne 2021 — Modélisation d'une coupe tuyau à cliquet</h2>
-            <ul>
-                <li>Modéliser des pièces avec SolidWorks.</li>
-            </ul>
-        </section>
-    </div>
-)};
+        <div id="study-projects-tab" class="tab">
+            <section class="study-projects">
+                <h1>PROJETS D'ÉTUDES</h1>
+                <hr />
+                <h2>Été 2023 — Prototypage de Moodle</h2>
+                <ul>
+                    <li>Implémenter des tests avec Jest;</li>
+                    <li>Programmer en TypeScript / JavaScript;</li>
+                    <li>Utiliser Node.js pour le côté serveur;</li>
+                    <li>Collaborer et partager le code avec les collègues via GitHub.</li>
+                </ul>
+                <h2>Hiver 2023 — TripleVue</h2>
+                <ul>
+                    <li>Implémenter des options de translation, zoom, glisser et déposer pour les images; </li>
+                    <li>Créer une barre de menu.</li>
+                </ul>
+                <h2>Hiver 2022 — Gestion des opérations maintenances</h2>
+                <ul>
+                    <li>Insérer des données avec SQL;</li>
+                    <li>Développer avec PL/SQL;</li>
+                    <li>Utiliser Oracle SQL Developer;</li>
+                    <li>Utiliser DataGrip;</li>
+                    <li>Créer des scripts SQL.</li>
+                </ul>
+                <h2>Hiver 2022 — Prototypage de jeu de rôle</h2>
+                <ul>
+                    <li>Générer un labyrinthe;</li>
+                    <li>Créer une interface graphique;</li>
+                    <li>Programmer en Java.</li>
+                </ul>
+                <h2>Hiver 2022 — Automatisation d'une grue</h2>
+                <ul>
+                    <li>Modéliser des pièces avec SolidWorks;</li>
+                    <li>Automatiser d'une grue en langage C;</li>
+                    <li>Utiliser d'AutoCAD pour la découpe au laser;</li>
+                    <li>Configurer la manette de contrôle pour la grue;</li>
+                    <li>Collaborer avec des partenaires de différents domaines pour favoriser
+                        l'apprentissage
+                        collectif.</li>
+                </ul>
+                <h2>Automne 2021 — Modélisation d'une coupe tuyau à cliquet</h2>
+                <ul>
+                    <li>Modéliser des pièces avec SolidWorks.</li>
+                </ul>
+            </section>
+        </div>
+    )
+};
 
 export default StudyProjects;
